3adbd5eb1671484d460d92e1ce739d34551fb448
Lukasz Kujawa <lukasz.kujawa@interia.pl>
2018-05-09 19:47:44 +0200

    change after codereview

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,10 +1,12 @@
-function getButtonClass() {
+function getButtonsClass() {
 	var navigation = document.getElementById('nav');
 	var elementsWithButtonClassinNav = navigation.getElementsByClassName('button');
 	return elementsWithButtonClassinNav;
 }
-
-var x = getButtonClass();
-console.log(x);	
-var y = x.length; 
-console.log(y);
+function showElementsContent(buttons,amount){
+	for(var i = 0; i < amount; i++){
+		alert(buttons[i].innerText);
+	}
+}
+var btns = getButtonsClass();
+showElementsContent(btns, btns.length);
